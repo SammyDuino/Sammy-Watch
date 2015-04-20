227fea52f0946ece3105e40ff1d4d5b172457e07
PacksGamingHD <craftyswans@gmail.com>
2015-04-19 18:06:35 -0700

    This commit fixes and error and adds to Release 1.1

--- TinyScreenClockAndDisplay/TinyScreenClockAndDisplay.ino
+++ TinyScreenClockAndDisplay/TinyScreenClockAndDisplay.ino
@@ -29,10 +29,10 @@ void setup(void) { // setup
   RTC.start();
   display.fontColor(GREEN,BLACK);
   display.setFont(liberationSans_16ptFontInfo);
-  display.setCursor(0,30);
+  display.setCursor(10);
   display.print("Watch");
   delay(2000);
-  display.clearWindow(x,x,x,x);
+  display.clearWindow(0,0,0,0);
 }
 
 void loop() {
